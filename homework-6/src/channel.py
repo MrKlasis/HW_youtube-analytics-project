b6dd675cf0d38b69ddca75165721119f9a3c52cc
Klasis <d23kv321@gmail.com>
2023-08-14 04:17:07 +0300

    commit

--- homework-6/src/channel.py
+++ homework-6/src/channel.py
@@ -57,7 +57,7 @@ class Channel:
 
     @classmethod
     def get_service(cls):
-        api_key: str = os.getenv('YOUTUBE_API')
+        api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         return youtube
 
--- homework-6/src/video.py
+++ homework-6/src/video.py
@@ -9,12 +9,19 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id).execute()
-        self.video_title: str = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f"https://www.youtube.com/{self.video_id}"
-        self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id).execute()
+            self.video_title: str = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f"https://www.youtube.com/{self.video_id}"
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
+        except Exception as e:
+            print(f"An error occurred while fetching video data: {e}")
+            self.video_title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.video_title
